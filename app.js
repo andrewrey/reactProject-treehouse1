1e41234ab4dc8baa28161afca966ce2bddc1f7fb
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-14 17:13:01 -0700

    created player and counter component

--- app.js
+++ app.js
@@ -6,4 +6,21 @@ const Header = () => {
     </header>
   );
 };
-ReactDOM.render(<Header />, document.getElementById("root"));
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Andrew</span>
+    </div>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement">-</button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment">+</button>
+    </div>
+  );
+};
+ReactDOM.render(<Counter />, document.getElementById("root"));
