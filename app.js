fda47eb05e709d1ae0acb666196725bcec9064e3
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-14 17:07:39 -0700

    created header component

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const title = React.createElement("h1", { id: "main-title", title: "This is a title." }, "My First React Element");
-const desc = React.createElement("p", null, "I just learned how to create a React node and render it into the DOM");
-const header = React.createElement("header", null, title, desc);
-ReactDOM.render(header, document.getElementById("root"));
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+};
+ReactDOM.render(<Header />, document.getElementById("root"));
